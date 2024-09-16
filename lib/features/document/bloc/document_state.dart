dacac70e5d6aebd15fcc97e256e183f57cd0a628
samakunchan <cedric.badjah@gmail.com>
2024-09-16 11:21:11 +0200

    feat(bloc): document
    - Ajout du systeme de state management pour les documents.

---
+++ lib/features/document/bloc/document_state.dart
@@ -0,0 +1,53 @@
+part of 'document_bloc.dart';
+
+abstract class DocumentState extends Equatable {
+  const DocumentState();
+}
+
+final class DocumentInitial extends DocumentState {
+  @override
+  List<Object> get props => [];
+}
+
+final class DocumentsAreLoading extends DocumentState {
+  @override
+  List<Object> get props => [];
+}
+
+final class DocumentsFormIsProcessing extends DocumentState {
+  @override
+  List<Object> get props => [];
+}
+
+// TODO Changer pour `DocumentsHaveFailures`
+final class DocumentsAreNotLoaded extends DocumentState {
+  const DocumentsAreNotLoaded({this.message = ErrorMessage.noErrorMessageHandled});
+
+  /// Tout les messages d'érreurs:
+  /// - [ErrorMessage.noErrorMessageHandled]
+  /// - [ErrorMessage.networkOfflineMessage]
+  /// - [ErrorMessage.serverFailureMessage]
+  /// - [ErrorMessage.cacheFailureMessage]
+  /// - [ErrorMessage.notFoundMessage]
+  /// - [ErrorMessage.unAuthorizationMessage]
+  /// - [ErrorMessage.forbiddenMessage]
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+final class DocumentFormIsSubmittedSuccessfully extends DocumentState {
+  const DocumentFormIsSubmittedSuccessfully({required this.documents});
+
+  final List<DocumentModel> documents;
+
+  @override
+  List<Object> get props => [];
+}
+
+class DocumentFormIsDeletedSuccessfully extends DocumentState {
+  const DocumentFormIsDeletedSuccessfully();
+  @override
+  List<Object> get props => [];
+}
