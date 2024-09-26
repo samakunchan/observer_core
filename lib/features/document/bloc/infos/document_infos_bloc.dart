a5b08e8b325618d1e5cfee9250cd9e9aade482d4
samakunchan <cedric.badjah@gmail.com>
2024-09-26 12:30:45 +0200

    fix: path

--- lib/features/document/bloc/infos/document_infos_bloc.dart
+++ lib/features/document/bloc/infos/document_infos_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:observer_core/models/models_export.dart';
 
 part 'document_infos_event.dart';
