c7f3e80064829df92b2f705997f6ab904d348ff6
samakunchan <cedric.badjah@gmail.com>
2024-09-26 12:04:21 +0200

    docs: get param
    - Documentation du get param

--- lib/features/server_nestjs/data/models/get_params.dart
+++ lib/features/server_nestjs/data/models/get_params.dart
@@ -1,10 +1,70 @@
 import 'package:equatable/equatable.dart';
+import 'package:observer_core/enums/enums.dart';
 
+/// # Récupération des données.
+/// ## Récupération d'une liste
+/// ```dart
+/// // Input
+/// GetParams(
+///   accessToken: authTokenModel.accessToken,
+///   endPoint: MainProject.documentsUploadEndPoint,
+/// ),
+/// ```
+/// ```
+/// // Output
+/// GET http://localhost:3005/documents
+/// ```
+/// ## Récupération d'un résultat
+/// ```dart
+/// // Input
+/// GetParams(
+///   accessToken: authTokenModel.accessToken,
+///   id: '5',
+///   endPoint: MainProject.documentsUploadEndPoint,
+/// ),
+/// ```
+/// ```
+/// // Output
+/// GET http://localhost:3005/documents/5
+/// ```
 class GetParams extends Equatable {
-  const GetParams({required this.accessToken, required this.endPoint});
+  /// # Récupération des données.
+  /// ## Récupération d'une liste
+  /// ```dart
+  /// // Input
+  /// GetParams(
+  ///   accessToken: authTokenModel.accessToken,
+  ///   endPoint: MainProject.documentsUploadEndPoint,
+  /// ),
+  /// ```
+  /// ```
+  /// // Output
+  /// GET http://localhost:3005/documents
+  /// ```
+  /// ## Récupération d'un résultat
+  /// ```dart
+  /// // Input
+  /// GetParams(
+  ///   accessToken: authTokenModel.accessToken,
+  ///   id: '5',
+  ///   endPoint: MainProject.documentsUploadEndPoint,
+  /// ),
+  /// ```
+  /// ```
+  /// // Output
+  /// GET http://localhost:3005/documents/5
+  /// ```
+  const GetParams({
+    required this.accessToken,
+    required this.endPoint,
+    this.id,
+    this.filteredBy,
+  });
 
   final String accessToken;
   final String endPoint;
+  final String? id;
+  final DocumentFilteredType? filteredBy;
 
   @override
   List<Object> get props => [accessToken, endPoint];
