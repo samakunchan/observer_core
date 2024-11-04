bfa911d7110a0a8ad0d45cced346ff80c1d81af7
samakunchan <cedric.badjah@gmail.com>
2024-11-04 18:28:13 +0100

    chore(article): model
    - Suppression du model.

--- lib/models/category/category_model.g.dart
+++ lib/models/category/category_model.g.dart
@@ -11,10 +11,6 @@ _$CategoryModelImpl _$$CategoryModelImplFromJson(Map<String, dynamic> json) =>
       title: json['title'] as String,
       content: json['content'] as String,
       id: (json['id'] as num?)?.toInt(),
-      articles: (json['articles'] as List<dynamic>?)
-              ?.map((e) => ArticleModel.fromJson(e as Map<String, dynamic>))
-              .toList() ??
-          ArticleModel.emptyList,
       projects: (json['projects'] as List<dynamic>?)
               ?.map((e) => ProjectModel.fromJson(e as Map<String, dynamic>))
               .toList() ??
@@ -30,7 +26,6 @@ Map<String, dynamic> _$$CategoryModelImplToJson(_$CategoryModelImpl instance) =>
       'title': instance.title,
       'content': instance.content,
       'id': instance.id,
-      'articles': instance.articles,
       'projects': instance.projects,
       'environment': instance.environment,
     };
